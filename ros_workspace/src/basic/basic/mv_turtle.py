c2c9d59f1de6a5a9d5e3fab4f9fd9d52c6d35277
ACUBCU <ldh9632@gmail.com>
2026-07-21 17:21:45 +0900

    turtle

--- ros_workspace/src/basic/basic/mv_turtle.py
+++ ros_workspace/src/basic/basic/mv_turtle.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Color, Pose
 # 선행 실행 명령어
 # ros2 run turtlesim turtlesim_node
 
@@ -9,7 +10,11 @@ class MV_turtle(Node):
         super().__init__("move_turtle")
         self.create_timer(0.1, self.timer_callback)
         self.pub = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
+        self.create_subscription(Pose, "turtle1/pose", self.pose_callback, 10)
+        self.create_subscription(Color, "turtle1/color_sensor", self.color_callback, 10)
         self.count = 0
+        self.pose = Pose()
+        self.color = Color()
 
     def timer_callback(self):
         msg = Twist()
@@ -17,6 +22,12 @@ class MV_turtle(Node):
         msg.angular.z = 1.0
         self.pub.publish(msg)
         self.count += 0.01
+
+    def pose_callback(self, msg : Pose):
+        self.pose = msg
+    
+    def color_callback(self, msg : color):
+        self.color = msg
 
 def main(args=None):
     rclpy.init(args=args)

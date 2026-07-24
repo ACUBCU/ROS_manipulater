21cdff000ce99b61fa6f74e2fabc88fa3430765e
ACUBCU <ldh9632@gmail.com>
2026-07-24 15:07:55 +0900

    urdf_display

--- ros_workspace/src/tf2_basic/setup.py
+++ ros_workspace/src/tf2_basic/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'tf2_basic'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ("share/" + package_name + "/launch", glob(os.path.join("launch", "*.launch.py"))),
+        ("share/" + package_name + "/urdf", glob(os.path.join("urdf", "*.*"))),
+        ("share/" + package_name + "/rviz", glob(os.path.join("rviz", "*.*"))),
+        ("share/" + package_name + "/meshes", glob(os.path.join("meshes", "*.*"))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
